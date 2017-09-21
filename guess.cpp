93c07d585a5b9cfee65b6a6420e049d401426397
rramos20 <rramos20@mail.csuchico.edu>
2017-09-20 23:24:56 -0700

    makes basic requirements

---
+++ guess.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()  {
+
+srand ( time(NULL) );
+int randomNumber;
+randomNumber = rand() % 10+1;
+int Guess;
+int AbsValue;
+int Difference;
+    cout<<"Guess the Computer's Number.(Between 1-100)"<<endl;
+
+cin>>Guess;
+if (Guess==randomNumber) {
+
+cout<<"Wow! You guessed it! You win!"<<endl;
+} 
+else if(Guess!=randomNumber);{ 
+Difference = Guess - randomNumber;
+AbsValue = abs(Difference);
+
+cout<<"You were off by "<<AbsValue<<". Sorry, you lose."<<endl;
+}
+
+cout<<"My number was "<<randomNumber<<endl;
+
+ return 0;
+
+}
